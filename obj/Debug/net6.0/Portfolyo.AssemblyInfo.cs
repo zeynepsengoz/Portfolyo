668ea5dc8825583e1aba7996d482ee3b45226185
zeynepsengoz <zeynepsengoz05@gmail.com>
2026-03-08 21:20:22 +0300

    fixxxx

--- obj/Debug/net6.0/Portfolyo.AssemblyInfo.cs
+++ obj/Debug/net6.0/Portfolyo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolyo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+360ae68c91e69d62e6322a75a3e20b860d1fe073")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e83608acf57c8e2867b282bfe90df97340bc80b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolyo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolyo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
